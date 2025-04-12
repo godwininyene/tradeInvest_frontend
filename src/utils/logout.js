fae05280685d68d849f0fc4fb020b8fafa388f69
godwininyene <godwinhigh2@gmail.com>
2025-04-12 06:55:38 -0700

    WIP: Remove active user when logged out.

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -3,6 +3,7 @@ export const logout = async (navigate) => {
     try {
         const res = await axios.get('/api/v1/users/logout');
         if (res.data.status === 'success') {
+            localStorage.removeItem("user");
             navigate('/users/login');
         }
     } catch (err) {
